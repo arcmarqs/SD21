adecace5e861968688bb4613e0be7867eb0a7395
Eduardo Santos <edu.2000.kill@gmail.com>
2022-01-05 23:47:17 +0000

    ja god foste
    Co-authored-by: Amadeu Marques <maramadeu@gmail.com>

--- src/ds/trabalho/parte3/Peer.java
+++ src/ds/trabalho/parte3/Peer.java
@@ -1,18 +1,12 @@
-package ds.trabalho.parte2;
+package ds.trabalho.parte3;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import io.grpc.Server;
-
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
 import java.util.Scanner;
-import java.util.regex.Pattern;
 
 public class Peer {
 
@@ -20,7 +14,6 @@ public class Peer {
     // ler o ip ou hostanme do seguinto
 
     public static void main(String[] args) throws Exception {
-        System.out.printf("new peer @ host=%s\n", args[0]);
         new Thread(new Server(args[0], args[1])).start();
         new Thread(new Client()).start();
     }
@@ -28,14 +21,14 @@ public class Peer {
 
 class Server implements Runnable {
     static final int PORT = 54545;
+
     ServerSocket server;
-    
+
     static boolean lock = false;
 
-    HashSet<String> iptable = new HashSet<>();
-    HashMap<String,String> dictionary = new HashMap<>(); 
-
+    static int token = 0;
     static String nextHost;
+
     public Server(String myHost, String nextHost) throws Exception {
         this.server = new ServerSocket(Server.PORT, 1, InetAddress.getByName(myHost));
         Server.nextHost = nextHost;
@@ -53,15 +46,28 @@ class Server implements Runnable {
         }
     }
 
-    public void register(String target){
-
-        iptable.add(target);
-
-
-        
+    public static void lock() {
+        Server.lock = true;
     }
 
+    public static void unlock() {
+        try {
+            System.out.println("Token " + Server.token);
+            
+            Socket socket = new Socket(InetAddress.getByName(nextHost), Server.PORT);
 
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+
+            out.println(String.valueOf(Server.token + 1));
+            out.flush();
+
+            socket.close();
+
+            Server.lock = false;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
 
 class Connection implements Runnable {
@@ -73,18 +79,16 @@ class Connection implements Runnable {
 
     @Override
     public void run() {
-        // prepare socket I/O channels
         try {
             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
             // parse command
             Scanner sc = new Scanner(in.readLine());
+            Server.token = Integer.parseInt(sc.next());
             sc.close();
 
             // close connection
             clientSocket.close();
-            
-            System.out.println("Token");
 
             if(!Server.lock) {
                 Server.unlock();
@@ -105,24 +109,15 @@ class Client implements Runnable {
 
     @Override
     public void run() {
-        String host;
         while (true) {
-
-            System.out.print("$ ");
-
-            String line = scanner.nextLine();
-            if(Pattern.matches("register(*)", line)){
-                host = line.substring(8, line.length() - 1);
-                Server.register(host);
-                
-            } else if(Pattern.matches("push(*)", line)) {
-                host = line.substring(5,line.length()-1);
-            } else if(Pattern.matches("pull(*)", line)) {
-                host = line.substring(5,line.length()-1);
-            } else if(Pattern.matches("pushpull(*)", line)) {
-                host = line.substring(9,line.length()-1);
+            switch (scanner.nextLine()) {
+                case "lock()":
+                    Server.lock();
+                    break;
+                case "unlock()":
+                    Server.unlock();
+                    break;
             }
-
         }
     }
 }
